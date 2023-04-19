f7d4b8a721040f2b26be7b442c6d90e409230b8e
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-04-19 17:41:24 +0530

    first commit

---
+++ patterns/hollow_rectangle.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class hollow_rectangle
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a length : ");       
+        int l=sc.nextInt();
+        System.out.print("Enter a breadth : ");       
+        int b=sc.nextInt();
+        for(int i=0; i<l; i++)
+        {
+            for(int j=0; j<b; j++)
+            {
+                if(i==0 || i==l-1 || j==0 || j==b-1)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}

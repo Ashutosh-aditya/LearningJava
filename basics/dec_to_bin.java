587e484d8be20b68fd80572c6b0dd8ff3aab5a7f
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-04-20 19:42:22 +0530

    .

---
+++ basics/dec_to_bin.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class dec_to_bin {
+
+    public static void dec2bin(int n)
+    {
+        int num=n,res=0,i=0;
+        while(num>0)
+        {
+            int d=num%2;
+            res=res*10+d;
+            num=num/2;
+            i++;
+        }
+        System.out.println("The binary of "+n+" is "+res);
+    }
+
+    public static void main(String[] args) {
+        try
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter a num : ");
+            int n=sc.nextInt();
+            dec2bin(n);
+            sc.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+    
+}

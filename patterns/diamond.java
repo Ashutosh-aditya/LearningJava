ea83cd61cf3616559280c3923dcb6aee0f29a669
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-04-23 23:01:29 +0530

    .

--- patterns/diamond.java
+++ patterns/diamond.java
@@ -4,7 +4,7 @@ public class diamond {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter no. of lines : ");
         int n = sc.nextInt();
-        int c=n-1;
+        int c=n-1,l=0,str=0;
         for (int i = 0; i <2*n; i++)
         {
             for( int j = 0; j <(n+i); j++)
@@ -20,19 +20,20 @@ public class diamond {
                         System.out.print(" * ");
                     }
                 }
-                // else
-                // {
-                //     if()
-                //     {
-                //         System.out.print(" * ");
-                //     }
-                //     else
-                //     {
-                //         System.out.print("   ");
-                //     }
-                // }
+                else
+                {
+                    if(j<=(l-n) || j>n-1 )
+                    {
+                        System.out.print("   ");
+                    }
+                    else
+                    {
+                        System.out.print(" * ");
+                    }
+                }
             }
             System.out.println();
+            l++;
             if(c==0)
             c=n;
         }

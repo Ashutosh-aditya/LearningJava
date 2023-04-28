bf5a31352018ea2494ee6b9f8731951f0d419137
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-04-28 23:12:28 +0530

    .

--- basics/subarrays.java
+++ basics/subarrays.java
@@ -1,15 +1,19 @@
+import java.util.*;
 public class subarrays {
-    public static int ts=0;
+    public static int ts=0,max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
     public static void pString(int ar[],int start,int end)
     {
-        
+        int sum=0;
         System.out.print("( ");
         for(int i = start; i <= end; i++)
             {
                 System.out.print(ar[i]+" ");
+                sum=ar[i]+sum;
             }
             System.out.print(")  ");
             ts++;
+        if(sum>max) max=sum;
+        if(sum<min) min=sum;
     }
     public static void main(String[] args) {
         int ar[]={1,2,3,4,5,6,7,8,9,10};
@@ -23,5 +27,6 @@ public class subarrays {
             System.out.println(" ");
         }
         System.out.println("Total subArrays : "+ts);
+        System.out.println("Max Sum : "+max+"\nMin Sum : "+min+"\n");
     }
 }

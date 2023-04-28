374ad663b29b47f935b1a3634b8a80473204c0b3
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-04-29 01:12:08 +0530

    Modified kadan's algo

--- basics/kadans_algo.java
+++ basics/kadans_algo.java
@@ -2,18 +2,25 @@ public class kadans_algo {
 
     public static void kadans(int ar[])
     {
-        int ms=Integer.MIN_VALUE,cs=0;
+        int ms,cs,s=ar[0];
+        for(int i=0;i<ar.length;i++)
+        {
+            if(ar[i]>s) s=ar[i];
+        }
+        cs=ms=s;
+        System.out.println("s="+s);
         for(int i=0;i<ar.length;i++)
         {
             cs=cs+ar[i];
-            if ((cs)<0) cs=0;
             if (cs>ms) ms=cs;
+            if ((cs)<0) cs=0;
         }
         System.out.println("Maximum subArray sum : "+ms);
     }
 
     public static void main(String[] args) {
-        int ar[]={1,-2,6,-1,3};
-        kadans(ar);
+        int ar[]={1,-2,6,-1,3},ar1[]={-1,-2,-3,-5};
+        // kadans(ar);
+        kadans(ar1);
     }
 }

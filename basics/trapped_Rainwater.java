b59eb0399f7cf2a1207d73f4a4ecca91333dd6f1
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-05-01 02:15:29 +0530

    testcase

--- basics/trapped_Rainwater.java
+++ basics/trapped_Rainwater.java
@@ -64,7 +64,17 @@ public class trapped_Rainwater {
 
 
     public static void main(String[] args) {
-        int ar[]={4,2,0,6,3,2,5};
+        int ar[]={4,2,0,6,3,2,5};                           //11
+        int[] arr1 = {2, 0, 2};                             //2
+        int[] arr2 = {3, 0, 0, 2, 0, 4};                    //10
+        int[] arr3 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};  //6
+        int[] arr4 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};        //0
+        int[] arr5 = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};        //0
         System.out.println("Total RainWater Accumulated : "+rainWater(ar));
+        System.out.println("Total RainWater Accumulated : "+rainWater(arr1));
+        System.out.println("Total RainWater Accumulated : "+rainWater(arr2));
+        System.out.println("Total RainWater Accumulated : "+rainWater(arr3));
+        System.out.println("Total RainWater Accumulated : "+rainWater(arr4));
+        System.out.println("Total RainWater Accumulated : "+rainWater(arr5));
     }
 }

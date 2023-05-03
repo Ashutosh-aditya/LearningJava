45a3e697baf59e47625ec6088978f40096793086
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-05-03 20:30:10 +0530

    spiral matrix done

--- basics/spiral_matrix.java
+++ basics/spiral_matrix.java
@@ -1,31 +1,41 @@
 public class spiral_matrix {
+    // for square matrix
     public static void solution( int mat[][])
     {
         int startRow=0,startCol=0;
         int endRow=mat.length-1;
         int endCol=mat[0].length-1;
+
         while(startRow<=endRow && startCol<=endCol)
         {
             for(int j=startCol;j<=endCol;j++)
             {
                 System.out.print(mat[startRow][j]+" ");
             }
+            // System.out.println("-1-");
 
             for(int i=startRow+1;i<=endRow;i++)
             {
                 System.out.print(mat[i][endCol]+" ");
             }
+            // System.out.println("-2-");
 
-            for(int j=endCol-1;j<=startCol;j--)
+            for(int j=endCol-1;j>=startCol;j--)
             {
-                System.out.print("---");
-                System.out.print(mat[startRow][j]+" ");
+                if(endCol==startCol)
+                break;
+                System.out.print(mat[endRow][j]+" ");
             }
+            // System.out.println("-3-");
 
-            for(int i=endRow-1;i<=startRow+1;i--)
+            for(int i=endRow-1;i>=startRow+1;i--)
             {
-                System.out.print(mat[i][endCol]+" ");
+                if(endRow==startRow)
+                break;
+                System.out.print(mat[i][startCol]+" ");
             }
+            // System.out.println("-4-");
+
             startRow++;
             startCol++;
             endCol--;
@@ -41,5 +51,7 @@ public class spiral_matrix {
                         {13,14,15,16}
                     };
         solution(mat);
+        
+        
     }
 }

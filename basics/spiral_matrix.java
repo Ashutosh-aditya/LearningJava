27af00f7a9c24d0b39ea0e307644f3d6e3c4c2f2
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-05-03 19:11:27 +0530

    spiral matrix

--- basics/spiral_matrix.java
+++ basics/spiral_matrix.java
@@ -1,38 +1,45 @@
 public class spiral_matrix {
-    public static void solution()
+    public static void solution( int mat[][])
     {
-        int mat[][] = new int[4][4],count=1;
-        for(int i = 0; i < 4; i++)
+        int startRow=0,startCol=0;
+        int endRow=mat.length-1;
+        int endCol=mat[0].length-1;
+        while(startRow<=endRow && startCol<=endCol)
         {
-            for(int j = 0; j < 4; j++)
+            for(int j=startCol;j<=endCol;j++)
             {
-                mat[i][j]=count++;
+                System.out.print(mat[startRow][j]+" ");
             }
-        }
 
-        for(int i = 0; i < 4; i++)
-        {
-            for(int j = 0; j < 4; j++)
+            for(int i=startRow+1;i<=endRow;i++)
             {
-                System.out.print(mat[i][j]+" ");
+                System.out.print(mat[i][endCol]+" ");
             }
-            System.out.println(" ");
-        }
 
-        for(int i = 0; i < 4; i++)
-        {
-            for(int j = 0; j < 4; j++)
+            for(int j=endCol-1;j<=startCol;j--)
             {
-                
+                System.out.print("---");
+                System.out.print(mat[startRow][j]+" ");
             }
-        }
 
+            for(int i=endRow-1;i<=startRow+1;i--)
+            {
+                System.out.print(mat[i][endCol]+" ");
+            }
+            startRow++;
+            startCol++;
+            endCol--;
+            endRow--;
+        }
     }
 
-    public static void printPatter(int mat[][],int i,int r,int c)
-    {}
 
     public static void main(String[] args) {
-        solution();
+        int mat[][]={   {1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}
+                    };
+        solution(mat);
     }
 }

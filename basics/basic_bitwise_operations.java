6983526f15512f7cae98eb0bd9f192bfed034066
“Ashutosh <adityaashutosh06@gmail.com>
2023-05-10 12:15:38 +0530

    formatting

--- basics/basic_bitwise_operations.java
+++ basics/basic_bitwise_operations.java
@@ -7,20 +7,28 @@ public class basic_bitwise_operations {
         System.out.print("Enter 2 numbers : ");
         a=sc.nextInt();
         b=sc.nextInt();
+        
         System.out.println(" ");
         System.out.println(a+" & "+b+" = "+(a&b));
+
         System.out.println(" ");
         System.out.println(a+" | "+b+" = "+(a|b));
+
         System.out.println(" ");
         System.out.println(a+" ^ "+b+" = "+(a^b));
+
         System.out.println(" ");
         System.out.println(a+"'s Compliment : "+(~a));
+
         System.out.println(" ");
         System.out.println(b+"'s Compliment : "+(~b));
+
         System.out.println(" ");
         System.out.println(a+" << "+b+" = "+(a<<b));
+
         System.out.println(" ");
         System.out.println(a+" >> "+b+" = "+(a>>b));
+
         System.out.println(" ");
         sc.close();
     }

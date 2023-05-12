eecd5bc1c6166ecd4ea2c3dad273e8f708ce3031
“Ashutosh <adityaashutosh06@gmail.com>
2023-05-12 13:05:45 +0530

    last occurance

---
+++ recursion/last_Occurance.java
@@ -0,0 +1,16 @@
+public class last_Occurance {
+
+    public static int firstOccurance(int ar[],int i,int key) {
+        if(ar.length==i+1)
+            return -1;
+        else if(ar[i]==key)
+            return i;
+        else 
+            return firstOccurance(ar, ++i, key);
+    }
+
+    public static void main(String[] args) {
+        int ar[]={1,2,3,9,5,6,7,8,9};
+        System.out.println(firstOccurance(ar, 0, 9 ));
+    }
+}

6adc08509194f1f38e3e6243dbbb0116355f915c
“Ashutosh <adityaashutosh06@gmail.com>
2023-05-17 19:17:43 +0530

    class member default value

---
+++ divide_and_conquer/one.java
@@ -0,0 +1,22 @@
+// class abc {
+//         int u;
+//         String str;
+//     abc()
+//     {
+        
+//         System.out.println(str);
+//     }
+    
+// }
+
+public class one {
+    static int y;
+    one()
+    {
+        System.out.println("y1="+y);
+    }
+    public static void main(String[] args) {
+        System.out.println("y="+y);
+    }
+
+}

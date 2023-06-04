5d8d0dc0fe7525ee74c63c1e6cf462f59768735f
“Ashutosh <adityaashutosh06@gmail.com>
2023-06-05 02:56:35 +0530

    power analysis for odd powers

--- divide_and_conquer/power_analysis_2.java
+++ divide_and_conquer/power_analysis_2.java
@@ -3,9 +3,14 @@ public class power_analysis_2 {
         if(n==1)
             return a;
         else
-            return (power(a,n/2)*power(a,n/2));
+            {
+                if(n%2==1)
+                    return (a*power(a,n/2)*power(a,n/2));
+                else 
+                    return (power(a,n/2)*power(a,n/2));
+            }
     }
     public static void main(String[] args) {
-        System.out.println(power(2,8));
+        System.out.println(power(2,9));
     }
 }

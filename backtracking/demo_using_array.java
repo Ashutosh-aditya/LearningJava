35421a49a92ed7b546370b73f1ba494891ea814e
“Ashutosh <adityaashutosh06@gmail.com>
2023-06-07 10:46:47 +0530

    demo of backtrack

--- backtracking/demo_using_array.java
+++ backtracking/demo_using_array.java
@@ -1,6 +1,30 @@
 
 public class demo_using_array {
+    public static void modArray(int ar[],int index)
+    {
+        if(index==5)
+        {
+            printArray(ar);
+            return;
+        }
+        // printArray(ar);
+        ar[index]=index+1;
+        modArray(ar,index+1);
+        // printArray(ar);
+        ar[index]-=2;
+        // printArray(ar);
+        // System.out.println(" ");
+    }
+    public static void printArray(int ar[]) {
+        for(int i=0;i<5;i++)
+        {
+            System.out.print(ar[i]+" ");
+        }
+        System.out.println(" ");
+    }
     public static void main(String[] args) {
-        
+        int array[] = new int[5];
+        modArray(array, 0);
+        printArray(array);
     }
 }

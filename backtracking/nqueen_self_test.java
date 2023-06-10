4b726d4f0da08ac6af024234af5d35f8b06b4d8b
“Ashutosh <adityaashutosh06@gmail.com>
2023-06-10 18:18:09 +0530

    nqueens full solution

--- backtracking/nqueen_self_test.java
+++ backtracking/nqueen_self_test.java
@@ -3,28 +3,51 @@ public class nqueen_self_test {
         int n=board.length;
         if(row==n)
         {
+            System.out.print("\n("+(++count)+") ");
             printBoard(board);
             return;
         }
         for(int i=0;i<n;i++)
         {
-            board[row][i]='Q';
-            nQueens(board, row+1);            
-            board[row][i]='X';            
+            if(isSafe(board,row,i)){
+                board[row][i]='Q';
+                nQueens(board, row+1);            
+                board[row][i]='X';     
+            }
         }
     }
+    private static boolean isSafe(char[][] board, int row, int col) {
+        //up condition
+        for(int i = row-1; i >=0 ;i--){
+            if(board[i][col]=='Q') 
+                return false;
+        }
+        //left up diagonal condition
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q')
+                return false;
+        }
+        //right up diagonal condition
+        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j]=='Q')
+                return false;
+        }
+        return true;
+    }
+    public static int count=0;
     public static void printBoard(char board[][]) {
         int n=board.length; 
         System.out.println("--------CHESS BOARD--------");
         for(int i=0;i<n;i++){
+                        System.out.print("\t");
             for(int j=0;j<n;j++){
-                System.out.print("\t"+board[i][j]+" ");
+                System.out.print(" "+board[i][j]+"");
             }
             System.out.println(" ");
         }
     }
     public static void main(String[] args) {
-        int n=3;
+        int n=10;
         char board[][]=new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){

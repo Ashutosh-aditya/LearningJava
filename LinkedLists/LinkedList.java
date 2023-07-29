455ca12e2e0f556c4caeb0f03bd9ce0573ef697d
“Ashutosh <adityaashutosh06@gmail.com>
2023-07-30 01:06:32 +0530

    add node removing functions

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -9,8 +9,10 @@ public class LinkedList {
         }
     }
     public static Node head,tail;
+    public int size=0;
 
     public void addNodeFirst(int data){
+        size++;
         Node n1 = new Node(data);
         if(head==null){
             head=tail=n1;
@@ -24,6 +26,7 @@ public class LinkedList {
 
     public void addNodeLast(int data){
         Node n1= new Node(data);
+        size++;
         if(head==null){
             head=tail=n1;
         }
@@ -36,9 +39,16 @@ public class LinkedList {
 
     public void addMid(int data,int index){
         Node n1=new Node(data);
-        Node temp=head;
+        Node temp=head.next;
         int i=0;
-        
+        if(size==0)
+        addNodeFirst(data);
+        for(i=0;i<index-2;i++){
+            temp=temp.next;
+        }
+        n1.next=temp.next;
+        temp.next=n1;
+        size++;
     }
 
     public void disp(){
@@ -53,6 +63,27 @@ public class LinkedList {
             System.out.println("null");
         }
     }
+
+    public void removeF(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        };
+        int d=head.data;
+        System.out.println(d+"has been removed");
+        head=head.next;
+        size--;
+        return;
+    }
+
+    public void removeL(){
+        Node temp=head;
+        for(int i=0;i<size-2;i++){
+            temp=temp.next;
+        }
+        temp.next=null;
+        tail=temp;
+    }
     
     public static void main(String[] args) {
         LinkedList ll =new LinkedList();
@@ -60,6 +91,12 @@ public class LinkedList {
         ll.addNodeFirst(1);
         ll.addNodeLast(3);
         ll.addNodeLast(4);
+        ll.disp();
+        ll.addMid(100, 3);
+        ll.disp();
+        ll.removeF();
+        ll.disp();
+        ll.removeL();
         ll.disp();
     }
 }

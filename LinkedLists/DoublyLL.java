e401a550e9dacbe1f4c433ba5f63e92b8b9ed720
“Ashutosh <adityaashutosh06@gmail.com>
2023-09-04 20:18:20 +0530

    Doubly Linked List

---
+++ LinkedLists/DoublyLL.java
@@ -0,0 +1,14 @@
+public class DoublyLL {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+        public Node(){
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    
+}

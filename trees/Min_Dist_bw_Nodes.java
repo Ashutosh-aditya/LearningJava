c9de89bca2d7e14326f6fc532ca6db0cd03b2a16
Ashutosh aditya <adityaashutosh6@gmail.com>
2024-03-21 13:03:39 +0530

    Minimum distance between 2 nodes

--- trees/Min_Dist_bw_Nodes.java
+++ trees/Min_Dist_bw_Nodes.java
@@ -1,6 +1,4 @@
-import java.util.*;
-public class LowestCommonAncestor {
-
+public class Min_Dist_bw_Nodes {
     static class Node{
         int data;
         Node left;
@@ -12,38 +10,33 @@ public class LowestCommonAncestor {
         }
     }
 
-    public static boolean getPath(Node root,int n,ArrayList<Node>path){
+    public static int lcaDist(Node root,int n){
         if(root==null){
-            return false;
+            return -1;
         }
-        path.add(root);
         if(root.data==n){
-            return true;
+            return 0;
         }
-        boolean foundleft=getPath(root.left, n, path);
-        boolean foundright=getPath(root.right, n, path);
-        if(foundleft || foundright){
-            return true;
+
+        int leftDist=lcaDist(root.left,n);
+        int rightDist=lcaDist(root.right,n);
+
+        if(leftDist==-1 && rightDist==-1){
+            return -1;
         }
-        path.remove(path.size()-1);
-        return false;
+        else if(leftDist==-1){
+            return rightDist+1;
+        }
+        else {
+            return leftDist+1;
+        }
     }
 
-    public static Node lca(Node root,int n1,int n2){ //TC:O(n) && SC:O(n)
-        ArrayList<Node> path1=new ArrayList<>();
-        ArrayList<Node> path2=new ArrayList<>();
-
-        getPath(root,n1,path1);
-        getPath(root,n2,path2);
-
-        int i=0;
-        for(;i<path1.size() && i<path2.size();i++){
-            if(path1.get(i)!=path2.get(i)){
-                break;
-            }
-        }
-        Node lca = path1.get(i-1);
-        return lca;
+    public static int minDist(Node root,int n1,int n2){
+        Node lca=lca2(root, n1, n2);
+        int dist1=lcaDist(lca,n1);
+        int dist2=lcaDist(lca,n2);
+        return dist1+dist2;
     }
 
     public static Node lca2(Node root,int n1,int n2){
@@ -55,10 +48,12 @@ public class LowestCommonAncestor {
 
         if(left==null)
             return right;
-        if(right==null)
+        else if(right==null)
             return left;
-        return root;
+        else 
+            return root;
     }
+
 
     public static void main(String[] args) {
         Node root = new Node(1);                        
@@ -76,7 +71,8 @@ public class LowestCommonAncestor {
               / \   / \
              4  5  6   7
          */
-        System.out.println(lca(root, 4, 5).data);
-        System.out.println(lca2(root, 4, 7).data);
+        System.out.println(minDist(root,4,7));
+        System.out.println(minDist(root,4,5));
+        System.out.println(minDist(root,4,6));
     }
 }

32bb91b7d00e16e1c9c4225129149f4de31d2340
Ashutosh aditya <adityaashutosh6@gmail.com>
2024-04-13 17:24:20 +0530

    basics of tries

--- Trie/tries_basics.java
+++ Trie/tries_basics.java
@@ -1,30 +1,80 @@
 public class tries_basics {
     static class Node{
-        Node children[];
+        Node children[] = new Node[26];
         Boolean endOfWord;
         Node(){
             for(int i=0;i<26;i++){
-                children[i]=null;
+                this.children[i]=null;
             }
             endOfWord=false;
         }
+    }
 
         static Node root=new Node();
         
 
-        public static void insert(String word,int idx,Node n){
-            if(idx==word.length()){
-                n.children[idx].endOfWord=true;
-                return;
+        // public static void insert(String word,int idx,Node n){
+        //     if(idx==word.length()){
+        //         n.children[idx].endOfWord=true;
+        //         return;
+        //     }
+        //     if(n.children[idx]==null){
+        //         Node n1 = new Node();
+        //     }
+        //     insert(word, idx+1, n.children[idx]);
+        // }
+
+        public static void insert(String str) { //O(L)  length of word
+            Node curr=root;
+            for(int level=0;level<str.length();level++){
+                int idx=str.charAt(level)-'a';
+                if(curr.children[idx]==null){
+                    curr.children[idx] = new Node();
+                }
+                curr=curr.children[idx];
             }
-        
-            if(n.children[idx]==null){
-                Node n1 = new Node();
-            }
-            insert(word, idx+1, n.children[idx]);
+            curr.endOfWord=true;
         }
+
+        public static boolean search(String str) { //O(L)  length of word
+            Node curr=root;
+            for(int level=0;level<str.length();level++){
+                int idx=str.charAt(level)-'a';
+                if(curr.children[idx]==null){
+                    return false;
+                }
+                curr=curr.children[idx];
+            }
+            if(curr.endOfWord!=true){
+                return false;
+            }
+            return true;
+        }
+
+        public static boolean wordBreak(String key) { //O(L)
+            if(key.length()==0){
+                return true;
+            }
+            for(int i=1;i<=key.length();i++){
+                if(search(key.substring(0,i)) && wordBreak(key.substring(i))){
+                    return true;
+                }
+            }
+            return false;
+        }
+
         public static void main(String[] args) {
-            
+            // String words[] = {"the","a","there","their","any","thee"};
+            String words1[]={ "i", "like", "sam", "samsung", "mobile", "ice"};
+            for(String s : words1) {
+                insert(s);
+            }
+            if(search("ther")){
+                System.out.println("Found");
+            }
+            else{
+                System.out.println("Not Found");
+            }
+            System.out.println(wordBreak("ilikesamsung"));
         }
-    }
 }
